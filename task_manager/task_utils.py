70ffa256a65b790f83e612fb6df757636420c876
timothydarrell14-glitch <timothydarrell14@gmail.com>
2026-06-23 14:30:52 +0300

    replaced marked as complete function with print message

--- task_manager/task_utils.py
+++ task_manager/task_utils.py
@@ -4,7 +4,10 @@ from datetime import datetime
 from task_manager.validation import (validate_due_date,validate_task_description, validate_task_title,)
 
 # Define tasks list
-tasks = []
+task = {"title": "Groceries",
+ "description": "Shop at Market Basket for food", 
+ "due_date": "2024-06-26",
+ "completed": True}
 
 # Implement add_task function
 def add_task(title, description, due_date):
@@ -12,7 +15,7 @@ def add_task(title, description, due_date):
     description = validate_task_description(description)
     due_date = validate_due_date(due_date)
 
-    tasks.append({
+    task.append({
         'title': title,
         'description': description,
         'due_date': due_date,
@@ -21,19 +24,19 @@ def add_task(title, description, due_date):
     print("Task added successfully!")
     
 # Implement mark_task_as_complete function
-def mark_task_as_complete(index, tasks=tasks):
-    task = tasks[index]
-    task['completed'] = True
+def mark_task_as_complete():
+    # task = tasks[index]
+    # task['completed'] = True
     print("Task marked as complete!")
     
 # Implement view_pending_tasks function
-def view_pending_tasks(tasks = tasks):
+def view_pending_tasks(tasks = task):
     for task in tasks:
         if task['completed'] == False:
             print(task)
 
 # Implement calculate_progress function
-def calculate_progress(tasks=tasks):
+def calculate_progress(tasks=task):
     completed = 0
     for task in tasks:
         if task['completed'] == True:
